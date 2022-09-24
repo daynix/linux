27d74efb3905a30cf9e5ba4632f6831f056d1d05
Akihiko Odaki <akihiko.odaki@gmail.com>
2022-09-24 18:42:02 +0900

    ASoC: amd: acp3x-5682: Expose individual headset jack pins
    The rt5682 codec is able to distinguish between two event types:
    headphone insertion/removal and headset microphone insertion/removal.
    However, currently, the acp3x-5682 driver exposes a single kcontrol
    for the headset jack, so userspace isn't able to differentiate between
    the two events.
    
    Add a definition for the headset jack pins, so that a separate jack
    kcontrol is created for each one, allowing userspace to track and handle
    them individually.
    
    This change is based on commit 194ff8db0378 ("ASoC: mediatek:
    mt8192-mt6359: Expose individual headset jack pins").
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@gmail.com>

--- sound/soc/amd/acp3x-rt5682-max9836.c
+++ sound/soc/amd/acp3x-rt5682-max9836.c
@@ -39,6 +39,17 @@ enum {
 	EC,
 };
 
+static struct snd_soc_jack_pin acp3x_5682_jack_pins[] = {
+	{
+		.pin	= "Headphone Jack",
+		.mask	= SND_JACK_HEADPHONE,
+	},
+	{
+		.pin	= "Headset Mic",
+		.mask	= SND_JACK_MICROPHONE,
+	},
+};
+
 static int acp3x_5682_init(struct snd_soc_pcm_runtime *rtd)
 {
 	int ret;
@@ -86,11 +97,12 @@ static int acp3x_5682_init(struct snd_soc_pcm_runtime *rtd)
 	rt5682_dai_wclk = clk_get(component->dev, "rt5682-dai-wclk");
 	rt5682_dai_bclk = clk_get(component->dev, "rt5682-dai-bclk");
 
-	ret = snd_soc_card_jack_new(card, "Headset Jack",
+	ret = snd_soc_card_jack_new_pins(card, "Headset Jack",
 				SND_JACK_HEADSET |
 				SND_JACK_BTN_0 | SND_JACK_BTN_1 |
 				SND_JACK_BTN_2 | SND_JACK_BTN_3,
-				&pco_jack);
+				&pco_jack, acp3x_5682_jack_pins,
+				ARRAY_SIZE(acp3x_5682_jack_pins));
 	if (ret) {
 		dev_err(card->dev, "HP jack creation failed %d\n", ret);
 		return ret;
